baf3d0f056aafcfecddd9885c27f79af47fcb6a9
FINALESHOT2016 <103410982+finaleshot2016@users.noreply.github.com>
2022-08-25 20:46:41 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,13 +13,13 @@ const About = () => {
           <div className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
 
             {/* Card */}
-            <AboutCard heading='Phase 1' text='Contract Deployment, Dogeswap Listing, Marketing Campaign & 200 Holders' />
+            <AboutCard heading='Phase 1' text='Contract Deployment, Uniswap Listing, Marketing Campaign & 200 Holders' />
             <AboutCard heading='Phase 2' text='Website Live, More Marketing Campaign, CMC and CG Listing & 500 Holders.' />
             <AboutCard heading='Phase 3' text='Website Upgrade, Dashboard, Whitepaper, More Marketing Campaign & 1,000 Holders.' />
             <AboutCard heading='Phase 4' text='Usecase Reveal & 5,000 Holders.' />
           </div>
           <div>
-<iframe className='py-4 w-full aspect-video' src="https://dexscreener.com/ethereum/0xdcab053acc1587ee625fd41bf44cbfa6e48708c2" title="dexchart"></iframe>
+<iframe className='py-4 w-full aspect-video' src="https://dexscreener.com/bsc/0x16b9a82891338f9ba80e2d6970fdda79d1eb0dae" title="dexchart"></iframe>
           </div>
         </div>
       </div>
